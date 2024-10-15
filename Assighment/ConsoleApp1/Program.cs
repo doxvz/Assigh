2b6e972bcfb4a5aef320a93df701b505f75f2440
doxvz <ludva7301@gmail.com>
2024-10-15 20:13:38 +0300

    Add files via upload

---
+++ Assighment/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Enter the first number:");
+int fN = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+int sN = int.Parse(Console.ReadLine());
+Console.WriteLine("Select opretor:");
+String oP = Console.ReadLine();
+
+if(oP == "+" || oP == "-" || oP == "" || oP == "/" )
+{
+    if(oP == "+")
+    {
+        Console.WriteLine("Result: " +fN+" + "+sN+" = "+(fN + sN));
+    }
+    else if(oP == "-")
+    {
+        Console.WriteLine("Result: " +fN+" - "+sN+" = "+(fN - sN));
+    }
+    else if(oP == "")
+    {
+        Console.WriteLine("Result: " +fN+" * "+sN+" = "+(fN * sN));
+    }
+    else if(oP == "/")
+    {
+        if(sN!=0)
+        {
+            Console.WriteLine("Result: " +fN+" / "+sN+" = "+(fN / sN));
+        }   
+        else
+        {
+            Console.WriteLine("Error! Division by zero is not allowed.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Wrong operator! Please use '+', '-', '', or '/' only.");
+    }
+
+}
+
+else{
+    Console.WriteLine("Wrong operator! Please use '+', '-', '', or '/' only.");
+}
